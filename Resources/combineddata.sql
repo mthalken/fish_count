df83858c3a45134691fdf516d7832e3a46bc24cb
unknown <lasitha.liyanage@msn.com>
2022-02-27 18:03:05 -0800

    update SQL files and adding ERD

---
+++ Resources/combineddata.sql
@@ -0,0 +1,23 @@
+INSERT INTO combineddata
+(
+select
+	dv.dateid,
+	dv.yearvalue,
+	dv.monthvalue,
+	dv.weeknumber,
+	'Bonneville' as locationname,
+	dv.datevalue,
+	bw.maxtempf,
+	bw.mintempf,
+	bw.precipitationinch,
+	bf.watertempf,
+	bf.stlheadcount
+from datevalues dv
+	left join
+		bonfishcounts bf
+		on dv.dateid = bf.countid
+	left join
+		bonweather bw
+		on dv.dateid = bw.countid
+);
+	
