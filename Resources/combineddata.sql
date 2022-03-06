887cb68b380bcbd5a8847f0f494221ed4f515bce
unknown <lasitha.liyanage@msn.com>
2022-03-06 12:54:12 -0800

    updated files to accomodate Heroku DB

--- Resources/combineddata.sql
+++ Resources/combineddata.sql
@@ -11,7 +11,38 @@ select
 	bw.mintempf,
 	bw.precipitationinch,
 	bf.watertempf,
-	bf.stlheadcount
+	bf.stlheadcount/*,
+	bf.chinookcount,
+	bf.sockeyecount,
+	bf.cohocount,
+	bf.chadcount*/
+from datevalues dv
+	left join
+		bonfishcounts bf
+		on dv.dateid = bf.countid
+	left join
+		bonweather bw
+		on dv.dateid = bw.countid
+);
+
+INSERT INTO combineddata2
+(
+select
+	dv.dateid,
+	dv.yearvalue,
+	dv.monthvalue,
+	dv.weeknumber,
+	'Bonneville' as locationname,
+	dv.datevalue,
+	bw.maxtempf,
+	bw.mintempf,
+	bw.precipitationinch,
+	bf.watertempf,
+	bf.stlheadcount,
+	bf.chinookcount,
+	bf.sockeyecount,
+	bf.cohocount,
+	bf.chadcount
 from datevalues dv
 	left join
 		bonfishcounts bf
